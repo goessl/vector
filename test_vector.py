7ae8a7b84ad61a8ced33e99cc1c09c5af8c96a42
Sebastian Gössl <goessl@student.tugraz.at>
2023-11-16 19:36:58 +0100

    Removed unnecessary test __main__
    Added basis vector test

--- test_vector.py
+++ test_vector.py
@@ -3,23 +3,23 @@ from math import isclose, sqrt
 
 
 
-if __name__ == '__main__':
-    assert isclose(abs(Vector.random(10)), 1)
-    assert not isclose(abs(Vector.random(10, normed=False)), 1)
-    assert abs(Vector.ZERO) == 0
-    
-    v = Vector((1, 2, 3, 4, 5))
-    
-    assert len(v) == 5
-    assert v[2] == 3
-    assert v[999] == 0
-    assert v<<1 == Vector((2, 3, 4, 5))
-    assert v>>1 == Vector((0, 1, 2, 3, 4, 5))
-    assert Vector((1, 0)).trim() == Vector((1,)) \
-            and Vector(tuple()).trim() == Vector(tuple())
-    
-    assert isclose(abs(v), sqrt(55))
-    assert v+Vector((3, 2, 1)) == Vector((4, 4, 4, 4, 5))
-    assert v-Vector((3, 2, 1)) == Vector((-2, 0, 2, 4, 5))
-    assert 2*v == v*2 == Vector((2, 4, 6, 8, 10))
-    assert v/2 == Vector((0.5, 1.0, 1.5, 2, 2.5))
+assert Vector(2) == Vector((0, 0, 1))
+assert isclose(abs(Vector.random(10)), 1)
+assert not isclose(abs(Vector.random(10, normed=False)), 1)
+assert abs(Vector.ZERO) == 0
+
+v = Vector((1, 2, 3, 4, 5))
+
+assert len(v) == 5
+assert v[2] == 3
+assert v[999] == 0
+assert v<<1 == Vector((2, 3, 4, 5))
+assert v>>1 == Vector((0, 1, 2, 3, 4, 5))
+assert Vector((1, 0)).trim() == Vector((1,)) \
+        and Vector(tuple()).trim() == Vector(tuple())
+
+assert isclose(abs(v), sqrt(55))
+assert v+Vector((3, 2, 1)) == Vector((4, 4, 4, 4, 5))
+assert v-Vector((3, 2, 1)) == Vector((-2, 0, 2, 4, 5))
+assert 2*v == v*2 == Vector((2, 4, 6, 8, 10))
+assert v/2 == Vector((0.5, 1.0, 1.5, 2, 2.5))
